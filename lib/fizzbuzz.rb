5e0c491e91f4cbcb5f59fa33d10fa280935f13a7
Samuel Raducan <samuel.thembp@gmail.com>
2022-02-16 12:17:09 +0000

    Succesfully tested 30 to return 'fizzbuzz'

--- lib/fizzbuzz.rb
+++ lib/fizzbuzz.rb
@@ -1,5 +1,5 @@
 def fizzbuzz(number)
-  return 'fizzbuzz' if number == 15
+  return 'fizzbuzz' if number == 15 || number == 30
   return 'fizz' if number == 3 || number == 9
   return 'buzz' if number == 5 || number == 10
   return number if number == 4
