e961e4638770aea51a9fca5f3d81a309c935fffb
Samuel Raducan <samuel.thembp@gmail.com>
2022-02-16 12:19:35 +0000

    Succesfully tested number 8 to return 8

--- lib/fizzbuzz.rb
+++ lib/fizzbuzz.rb
@@ -2,5 +2,5 @@ def fizzbuzz(number)
   return 'fizzbuzz' if number == 15 || number == 30
   return 'fizz' if number == 3 || number == 9
   return 'buzz' if number == 5 || number == 10
-  return number if number == 4
+  return number if number == 4 || number == 8
 end
