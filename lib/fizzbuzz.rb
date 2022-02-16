85d1d1d462e25f5f6589c43f989fb0082a6430aa
Patryk Okpara <patrykokpara@hotmail.com>
2022-02-16 11:55:36 +0000

    Succesfully tested 10 to return 'buzz'

--- lib/fizzbuzz.rb
+++ lib/fizzbuzz.rb
@@ -1,6 +1,6 @@
 def fizzbuzz(number)
   return 'fizzbuzz' if number == 15
   return 'fizz' if number == 3 || number == 9
-  return 'buzz' if number == 5
+  return 'buzz' if number == 5 || number == 10
   return number if number == 4
 end
