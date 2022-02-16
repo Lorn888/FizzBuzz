6381a5bbcc9016fbf1f6af31cb679ea63db3cc1e
Samuel Raducan <samuel.thembp@gmail.com>
2022-02-16 12:12:37 +0000

    Add test for 30 to return 'fizzbuzz'

--- spec/fizzbuzz_spec.rb
+++ spec/fizzbuzz_spec.rb
@@ -7,15 +7,21 @@ describe 'fizzbuzz' do
    it 'returns "fizz" when passed 9' do
     expect(fizzbuzz(9)).to eq 'fizz'
   end
+  
   it 'returns "buzz" when passed 5' do
     expect(fizzbuzz(5)).to eq 'buzz'
   end
    it 'returns "buzz" when passed 10' do
     expect(fizzbuzz(10)).to eq 'buzz'
   end  
+  
   it 'returns "fizzbuzz" when passed 15' do
     expect(fizzbuzz(15)).to eq 'fizzbuzz'
   end
+  it 'returns "fizzbuzz" when passed 30' do
+    expect(fizzbuzz(30)).to eq 'fizzbuzz'
+  end
+  
   it "returns the number when passed 4" do
     expect(fizzbuzz(4)).to eq 4
   end
